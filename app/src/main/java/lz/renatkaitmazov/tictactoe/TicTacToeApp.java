8015efdf064a1410ab81d64c16e42b987faae9f6
Renat Kaitmazov <renatkaitmazov@gmail.com>
2017-04-10 13:31:39 +0300

    Add application class

---
+++ app/src/main/java/lz/renatkaitmazov/tictactoe/TicTacToeApp.java
@@ -0,0 +1,24 @@
+package lz.renatkaitmazov.tictactoe;
+
+import android.app.Application;
+
+/**
+ * @author Renat Kaitmazov
+ */
+
+public final class TicTacToeApp extends Application {
+
+    /** Static variables **/
+
+
+    /** Lifecycle **/
+
+    @Override
+    public final void onCreate() {
+        super.onCreate();
+    }
+
+    /** API **/
+
+
+}
